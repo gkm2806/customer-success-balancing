1b93dfab6f41b6de3d1584a03e657d83ef579ff7
kukiel <gabrielkukiel@gmail.com>
2022-01-08 00:17:43 -0400

    [NEW] adding some of the premises to customerSuccessBalancing

--- javascript/src/models/customerSuccessBalancing.ts
+++ javascript/src/models/customerSuccessBalancing.ts
@@ -3,15 +3,44 @@
  * @param {array} customerSuccess
  * @param {array} customers
  * @param {array} customerSuccessAway
+ * ==== FLECHA ==> Having types is great, isn't it? :D
  */
- function customerSuccessBalancing(
-  customerSuccess,
-  customers,
-  customerSuccessAway
-) {
-  /**
-   * ===============================================
-   * =========== Write your solution here ==========
-   * ===============================================
-   */
-}
+const ensureRange = (len, min, max) => len > min && len < max;
+
+export const customerSuccessBalancing = (
+  customerSuccess: customerSuccess[],
+  customers: customers[],
+  customerSuccessAway: number[]
+) => {
+  const removeAwayCustomerSuccess = ():customerSuccess[] => (
+    customerSuccess.filter((cs) => customerSuccessAway.includes(cs.id))
+  );
+
+  const activeCustomersSuccess:customerSuccess[] = removeAwayCustomerSuccess();
+  const activeCustomers: customers[] = customers;
+
+  const ensureNoCustomersSuccessWithSameScore = () => {
+    const scoreArray:number[] = activeCustomersSuccess.map((acs) => acs.id);
+    const scoreSet = new Set(scoreArray);
+    if (scoreArray.length !== scoreSet.size) throw('No CSs with same score are allowed');
+  };
+
+  const ensureCustomersSuccessRange = () => {
+    if(!ensureRange(activeCustomersSuccess.length, 0, 1000)) throw('Css array out of range');
+  };
+
+  const ensureClientsRange = () => {
+    if(!ensureRange(activeCustomersSuccess.length, 0, 1000000)) throw('Client array out of range');
+  };
+
+  const ensureMaxAwayLimit = () => {
+    if(customerSuccessAway.length > customerSuccess.length) throw('Limit of away CSs breached');
+  };
+
+  ensureNoCustomersSuccessWithSameScore();
+  ensureCustomersSuccessRange();
+  ensureClientsRange();
+  ensureMaxAwayLimit();
+
+  //* END OF PREMISES *//
+};
