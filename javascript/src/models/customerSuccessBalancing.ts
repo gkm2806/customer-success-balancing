b24194e1ca953e8f1cebf6deb934faf8e06ab3be
kukiel <gabrielkukiel@gmail.com>
2022-01-08 02:04:29 -0400

    [FIX] simplifying things up

--- javascript/src/models/customerSuccessBalancing.ts
+++ javascript/src/models/customerSuccessBalancing.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /**
  * Returns the id of the CustomerSuccess with the most customers
  * @param {array} customerSuccess
@@ -5,7 +6,6 @@
  * @param {array} customerSuccessAway
  * ==== FLECHA ==> Having types is great, isn't it? :D
  */
-const ensureRange = (len, min, max) => len > min && len < max;
 
 export const customerSuccessBalancing = (
   customerSuccess: customerSuccess[],
@@ -13,35 +13,15 @@ export const customerSuccessBalancing = (
   customerSuccessAway: number[]
 ):number => {
   const removeAwayCustomerSuccess = ():customerSuccess[] => (
-    customerSuccess.filter((cs) => customerSuccessAway.includes(cs.id))
+    customerSuccess
+      .filter((cs) => !customerSuccessAway.includes(cs.id))
+      .sort((a,b) => a.score - b.score)
+      .map((cs) => ({ ...cs, customerCount: 0}))
   );
 
   const activeCustomersSuccess:customerSuccess[] = removeAwayCustomerSuccess();
   const activeCustomers: customers[] = customers;
 
-  const ensureNoCustomersSuccessWithSameScore = () => {
-    const scoreArray:number[] = activeCustomersSuccess.map((acs) => acs.id);
-    const scoreSet = new Set(scoreArray);
-    if (scoreArray.length !== scoreSet.size) throw('No CSs with same score are allowed');
-  };
-
-  const ensureCustomersSuccessRange = () => {
-    if(!ensureRange(activeCustomersSuccess.length, 0, 1000)) throw('Css array out of range');
-  };
-
-  const ensureClientsRange = () => {
-    if(!ensureRange(activeCustomersSuccess.length, 0, 1000000)) throw('Client array out of range');
-  };
-
-  const ensureMaxAwayLimit = () => {
-    if(customerSuccessAway.length > customerSuccess.length) throw('Limit of away CSs breached');
-  };
-
-  ensureNoCustomersSuccessWithSameScore();
-  ensureCustomersSuccessRange();
-  ensureClientsRange();
-  ensureMaxAwayLimit();
-
+  console.log('Esse são os ids válidos', activeCustomersSuccess.map((acs) => acs.id));
   return 0;
-  //* END OF PREMISES *//
 };
